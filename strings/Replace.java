ff4c6bc657f24df86141b828efec026d095ef335
CarolinaCedro <yotechgirl@gmail.com>
2022-07-06 12:34:12 -0300

    ++

---
+++ strings/Replace.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class Replace {
+
+    public static void main(String[] args){
+
+        String textOriginal = " ANA carolINA cEDro ", textModify;
+
+        textModify = textOriginal.replace("A","E");
+
+        System.out.println("texto original = "+textOriginal);
+        System.out.println("texto modificado com replace = "+textModify);
+    }
+}
