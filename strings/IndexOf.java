a9f26ca7ce8f97e7a61b34800b2c1a424cede11e
CarolinaCedro <yotechgirl@gmail.com>
2022-07-06 13:57:19 -0300

    ++

--- strings/IndexOf.java
+++ strings/IndexOf.java
@@ -1,4 +1,14 @@
 package strings;
 
 public class IndexOf {
+    public static void main(String[] args) {
+        String textOriginal = " ANA carolINA cEDro ";
+        int textModify;
+
+         textModify = textOriginal.indexOf("NA");
+
+        System.out.println("texto original = "+textOriginal);
+        System.out.println("texto modificado com indexOf = "+textModify);
+    }
+
 }
