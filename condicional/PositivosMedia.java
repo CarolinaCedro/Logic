050de90d0108887efd479da27a0f580d8ac1f49f
CarolinaCedro <yotechgirl@gmail.com>
2022-07-06 14:22:41 -0300

    +++++

---
+++ condicional/PositivosMedia.java
@@ -0,0 +1,52 @@
+package condicional;
+
+import java.util.Scanner;
+
+public class PositivosMedia {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double n1,n2,n3,n4,n5,n6,positivos=0,mediaPositivos=0;
+
+        n1 = sc.nextDouble();
+        if (n1 > 0){
+            positivos+=1;
+            mediaPositivos+=n1;
+
+        }
+
+        n2 = sc.nextDouble();
+        if (n2 > 0){
+            positivos+=1;
+            mediaPositivos+=n2;
+        }
+
+        n3 = sc.nextDouble();
+        if (n3 > 0){
+            positivos+=1;
+            mediaPositivos+=n3;
+        }
+
+        n4 = sc.nextDouble();
+        if (n4 > 0){
+            positivos+=1;
+            mediaPositivos+=n4;
+        }
+
+        n5 = sc.nextDouble();
+        if (n5 > 0){
+            positivos+=1;
+            mediaPositivos+=n5;
+        }
+
+        n6 = sc.nextDouble();
+        if (n6 > 0){
+            positivos+=1;
+            mediaPositivos+=n6;
+        }
+
+        System.out.printf("%.0f valores positivos %n",positivos);
+        System.out.printf("%.1f",mediaPositivos/positivos);
+    }
+}
