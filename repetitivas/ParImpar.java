57845cbf0862f07f309a0f2fc4622ff4e7b44933
CarolinaCedro <yotechgirl@gmail.com>
2022-07-06 21:39:41 -0300

    feat: ++

--- repetitivas/ParImpar.java
+++ repetitivas/ParImpar.java
@@ -1,4 +1,36 @@
 package repetitivas;
 
+import java.util.Scanner;
+
 public class ParImpar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+
+        N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+
+            int x = sc.nextInt();
+
+            if (x == 0){
+                System.out.println("NULL");
+            } else if (x % 2 == 0 && x < 0) {
+                System.out.println("EVEN NEGATIVE");
+            } else if (x % 2 != 0 && x < 0) {
+                System.out.println("ODD NEGATIVE");
+            } else if (x % 2 != 0 && x > 0) {
+                System.out.println("ODD POSITIVE");
+            }else {
+                System.out.println("EVEN POSITIVE");
+            }
+        }
+
+    }
 }
+
+
+
+
