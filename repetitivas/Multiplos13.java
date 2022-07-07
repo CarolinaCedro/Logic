6088ceebb2b907e91567df3a781c617291f4b18a
CarolinaCedro <yotechgirl@gmail.com>
2022-07-07 15:03:18 -0300

    +++

--- repetitivas/Multiplos13.java
+++ repetitivas/Multiplos13.java
@@ -6,6 +6,26 @@ public class Multiplos13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int X, Y, soma = 0, maior, menor;
 
+        X = sc.nextInt();
+        Y = sc.nextInt();
+
+        if (X > Y) {
+            menor = Y;
+            maior = X;
+        } else {
+            menor = X;
+            maior = Y;
+        }
+
+        for (int i = menor; i <=maior; i++) {
+
+            if (i % 13 != 0) {
+                soma+=i;
+            }
+        }
+
+        System.out.println(soma);
     }
 }
